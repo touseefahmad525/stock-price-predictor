81d3813cdfa7b9d533301f1027811590bf952fb0
Touseef <touseefahmadkhan789@gmail.com>
2026-05-02 23:29:18 +0500

    add URL

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://your-netlify-site.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
